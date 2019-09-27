6f7497c42175adc45f2144e17a775d87ee7fa26f
Lagrango <davidnlagrange@hotmail.com>
2019-09-27 14:10:23 -0500

    Initial Commit of Gesture Comparison Project Example

---
+++ gestureComparisonExample/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gestureComparisonExample
+{
+    class Game
+    {
+        Player playerOne;
+        Player playerTwo;
+
+        public Game()
+        {
+            playerOne = new Player() { name = "Ahnikka", score = 0, gesture = "scissors" };
+            playerTwo = new Player() { name = "David", score = 0, gesture = "paper" };
+        }
+
+        public void RunGame()
+        {
+            playerTwo = CompareGestureOption2(playerTwo, playerOne);
+            playerOne = CompareGestureOption2(playerOne, playerTwo);
+
+            Console.WriteLine($"Player One Score: {playerOne.score}\nPlayer Two Score: {playerTwo.score}");
+        }
+        public Player CompareGestureOption2(Player playerOne, Player playerTwo)
+        {
+            if (playerOne.gesture == "scissors" && (playerTwo.gesture == "paper" || playerTwo.gesture == "lizard"))
+            {
+                Console.WriteLine($"{playerOne.name} has won the round! Scissors cuts paper!");
+                playerOne.score++;
+            }
+            return playerOne;
+        }
+    }
+}
---
+++ gestureComparisonExample/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gestureComparisonExample
+{
+    class Player
+    {
+        public string name;
+        public int score;
+        public string gesture;
+    }
+}
